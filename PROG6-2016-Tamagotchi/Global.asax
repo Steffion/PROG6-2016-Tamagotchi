11f555d654398d10e5666926e538a91e773b0cee
Stef de Goey <stefdegoey@icloud.com>
2016-12-12 13:23:11 +0100

    First commit

---
+++ PROG6-2016-Tamagotchi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PROG6_2016_Tamagotchi.MvcApplication" Language="C#" %>
---
+++ PROG6-2016-Tamagotchi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PROG6_2016_Tamagotchi.Startup))]
+namespace PROG6_2016_Tamagotchi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
