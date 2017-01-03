fc71443bab64c1b98c3f46e98cc1d56babd2335f
Stef de Goey <stefdegoey@icloud.com>
2017-01-03 10:21:42 +0100

    Added database and WPF project

---
+++ Database/Tamagotchi.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Tamagotchi]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL, 
+    [Age] INT NULL DEFAULT 0, 
+    [Hunger] INT NULL DEFAULT 0, 
+    [Sleep] INT NULL DEFAULT 0, 
+    [Bored] INT NULL DEFAULT 0, 
+    [Health] INT NULL DEFAULT 100
+)
