45a96ba3a96b32b49c56c91b21d82a6bba42b675
Stef de Goey <stefdegoey@icloud.com>
2017-01-03 12:32:16 +0100

    Azure configured

---
+++ PROG6-2016-Tamagotchi/Models/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PROG6_2016_Tamagotchi.Models
+{
+    public class Database : DbContext
+    {
+        public Database() : base("name=Azure")
+        {
+            System.Data.Entity.Database.SetInitializer(new DatabaseInitializer());
+        }
+
+        public DbSet<Tamagotchi> Tamagotchis { get; set; }
+    }
+}
