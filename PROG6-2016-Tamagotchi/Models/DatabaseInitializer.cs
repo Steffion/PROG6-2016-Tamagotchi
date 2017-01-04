e633da3ca8cfed49df61ef6ac289af64f58e7ce2
Stef de Goey <stefdegoey@icloud.com>
2017-01-04 16:58:44 +0100

    Updated test data

--- PROG6-2016-Tamagotchi/Models/DatabaseInitializer.cs
+++ PROG6-2016-Tamagotchi/Models/DatabaseInitializer.cs
@@ -12,10 +12,15 @@ namespace PROG6_2016_Tamagotchi.Models
         {
             base.Seed(context);
 
-            context.Tamagotchis.Add(new Tamagotchi() { Name = "Ger", Created = DateTime.Now, LastAccess = DateTime.Now });
-            context.Tamagotchis.Add(new Tamagotchi() { Name = "Merel", Created = DateTime.Now, LastAccess = DateTime.Now });
-            context.Tamagotchis.Add(new Tamagotchi() { Name = "Stijn", Created = DateTime.Now, LastAccess = DateTime.Now });
-            context.Tamagotchis.Add(new Tamagotchi() { Name = "Stef", Created = DateTime.Now, LastAccess = DateTime.Now });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Ger", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Merel", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Stijn", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Stef", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Koen", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Guus", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Fred", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Rik", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
+            context.Tamagotchis.Add(new Tamagotchi() { Name = "Kim", Created = DateTime.Now, LastAccess = DateTime.Now, Health = 100 });
 
             context.SaveChanges();
         }
